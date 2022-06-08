1c1aee9889d3b484a0486e54c94123cedee5660e
transmagnati02 <ti02@transmagna.com.br>
2022-06-08 14:14:00 -0300

    melhorado arquivo de modelo de testes

--- exemplo_cqTester.php
+++ exemplo_cqTester.php
@@ -1,26 +1,42 @@
 <?php
   require_once __DIR__ . '/cqTester/cqTest.php';
+
+  class Exemplo {
+    
+    private $titulo;
+    
+    public function setTitulo($str) {
+      $this->titulo = $str;
+    }
+
+    public function isValido() {
+      if (empty($this->titulo)) {
+        return false;
+      }
+      return true;
+    }
+  }
+
   class Test_Exemplo extends cqTest {
 
     static function setup() {
-        user::logIn('username', 'password');
-        require_once __DIR__ . '/../src/Exemplo.php';
+        // Prepare o ambiente para que o teste consiga executar
+        // Requeira scripts, preencha variáveis de seção, etc
     }
 
-    static function mockUpExemplo($titulo, $descricao) {
+    static function mockUpExemplo($titulo) {
         $obj = new Exemplo;
         $obj->setTitulo($titulo);
-        $obj->setDescricao($descricao);
         return $obj;
     }
 
     static function testExemploValido() {
-        $obj = self::mockUpExemplo('Teste', 'Testando');
+        $obj = self::mockUpExemplo('Teste');
         assert($obj->isValido() === true, 'O exemplo deveria ser válido pois possui todos dados necessários');
     }
     
     static function testExemploInvalido() {
-        $obj = self::mockUpExemplo('Teste', '');
+        $obj = self::mockUpExemplo('Teste');
         assert($obj->isValido() === false, 'O exemplo deveria ser invalido pois é necessária uma descrição válida');
     }
     
